4752dc41a138ad65d0299831563f834a3952e448
ASR <saef.ratul@gmail.com>
2024-05-02 00:11:43 +0600

    dev

--- index.js
+++ index.js
@@ -25,10 +25,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
+    client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
     // Send a ping to confirm a successful connection
-    const database = await client.db("craftandArtDB");
-    const products = await database.collection("CraftProducts");
+    const database = client.db("craftandArtDB");
+    const products =  database.collection("CraftProducts");
 
 
 
